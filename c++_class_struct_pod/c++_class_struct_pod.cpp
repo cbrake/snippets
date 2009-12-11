5de103364b96922a6086d7c2243a08a19e302d3a
cbrake <cbrake@b420432d-d076-43ee-a9e1-cd6183467e5f>
2009-12-11 20:10:57 +0000

    add memset

--- c++_class_struct_pod/c++_class_struct_pod.cpp
+++ c++_class_struct_pod/c++_class_struct_pod.cpp
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 
 
 struct A
@@ -34,7 +35,11 @@ main()
 {
 	A a;
 	B b(1,2);
+	B b2(3,4);
+	
+	memset(&b2, 0, sizeof(b2));
 
 	printf("sizeof(a) = %i, sizeof(b) = %i\n", sizeof(a), sizeof(b));
+	printf("b2.get_a() returns %i\n", b2.get_a());
 }
 
