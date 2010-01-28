6a2f636585ddf692e41baa238a7f6cc01fb40e95
cbrake <cbrake@b420432d-d076-43ee-a9e1-cd6183467e5f>
2010-01-28 15:43:44 +0000

    update

--- cpp-vector/cpp-vector.cpp
+++ cpp-vector/cpp-vector.cpp
@@ -15,7 +15,6 @@ class point {
 			return "hi cliff";
 		}
 			
-	private:
 		int x;
 		int y;
 		int z;
@@ -39,6 +38,8 @@ main()
 	points.push_back(c);
 	points.push_back(c);
 	points_.push_back(&c);
+
+	printf("last point, x = %i\n", points.back().x);
 
 	// print all points
 	cout << "Loop by index, copy in vector" << endl;
