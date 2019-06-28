7d6381412f6cbe343e60ccc0b943ab6bf2a90dd4
Cliff Brake <cbrake@bec-systems.com>
2019-06-28 12:54:09 -0400

    update pass by value

--- go/pass-by-value.go
+++ go/pass-by-value.go
@@ -7,15 +7,24 @@ func test(data []int) {
 	fmt.Println("data in test: ", data)
 }
 
+func test2(data []int) {
+	data[0] = 10
+	fmt.Println("data in test2: ", data)
+}
+
 func main() {
 	a := []int{1, 2, 3}
-	fmt.Println("data before func: ", a)
+	fmt.Println("data before test(): ", a)
 	test(a)
-	fmt.Println("data after func: ", a)
+	fmt.Println("data after test(): ", a)
+	test2(a)
+	fmt.Println("data after test2(): ", a)
 }
 
 // program output:
 // go run pass-by-value.go
-// data before func:  [1 2 3]
+// data before test():  [1 2 3]
 // data in test:  [1 2 3 4]
-// data after func:  [1 2 3]
+// data after test():  [1 2 3]
+// data in test2:  [10 2 3]
+// data after test2():  [10 2 3]
