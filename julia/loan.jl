fed266b455d387916db1b70ef273a6b15c3ea10a
Cliff Brake <cbrake@bec-systems.com>
2022-05-02 10:28:36 -0400

    add program output

--- julia/loan.jl
+++ julia/loan.jl
@@ -10,3 +10,8 @@ loan_amount=337500
 monthly_payment = pmt(0.02/12, 30*12, 337500)
 
 @printf("Monthly payment for %d year loan at %f %% interest is \$ %f\n", term_years, interest_rate*100, monthly_payment)
+
+
+# programm output
+# $ julia loan.jl
+# Monthly payment for 30 year loan at 2.000000 % interest is $ -1247.465720
